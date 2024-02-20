2efed6bc900488941929e46c168cca4a7c1fa5ca
Kamran Babazadeh <99259950+CamCodesSwiftly@users.noreply.github.com>
2024-02-20 18:58:30 +0100

    feat: install multer, set up route and res.json
    This was fairly easy with multer.
    I installed it, the receiving of the file is easy.
    As long as the form has the correct enctype, all you need to do is
    to give the post route an extra argument called upload.single("whateverthenamefieldofthatformis")
    and you can receive the file with req.file.

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+	console.log(req.file);
+	res.json({
+		name: req.file.originalname,
+		type: req.file.mimetype,
+		size: req.file.size,
+	});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+	console.log("Your app is listening on port " + port);
 });
